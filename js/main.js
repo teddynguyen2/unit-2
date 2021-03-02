24f440c7b2a40bf43eccee95f78e74c3af30f42e
teddynguyen2 <61038278+teddynguyen2@users.noreply.github.com>
2021-03-01 21:44:39 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,177 +18,40 @@ function createMap(){
     getData(map);
 };
 
-function calculateMinValue(data){
-    //create empty array to store all data values
-    var allValues = [];
-    //loop through each city
-    for(var city of data.features){
-        //loop through each year
-        for(var year = 2010; year <= 2019; year+=1){
-              //get population for current year
-              var value = city.properties["Pop_"+ String(year)];
-              //add value to array
-              allValues.push(value);
+//added at Example 2.3 line 20...function to attach popups to each mapped feature
+function onEachFeature(feature, layer) {
+    //no property named popupContent; instead, create html string with all properties
+    var popupContent = "";
+    if (feature.properties) {
+        //loop to add feature property names and values to html string
+        for (var property in feature.properties){
+            popupContent += "<p>" + property + ": " + feature.properties[property] + "</p>";
+            
         }
     }
-    //get minimum value of our array
-    var minValue = Math.min(...allValues)
-
-    return minValue;
 }
 
-//calculate the radius of each proportional symbol
-function calcPropRadius(attValue) {
-    //constant factor adjusts symbol sizes evenly
-    var minRadius = 5;
-    //Flannery Apperance Compensation formula
-    var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
-
-    return radius;
-};
-
-//function to convert markers to circle markers and add popups
-function pointToLayer(feature, latlng, attributes){
-    //Determine which attribute to visualize with proportional symbols
-    var attribute = attributes[0];
-    //For each feature, determine its value for the selected attribute
-    var attValue = Number(feature.properties[attribute]);
-
-    //create marker options
-    var options = {
-        fillColor: "#ff7800",
-        color: '#000',
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8
-     };
-    
-    //Give each feature's circle marker a radius based on its attribute value
-    options.radius = calcPropRadius(attValue);
-    
-    //create circle marker layer
-    var layer = L.circleMarker(latlng, options);
-    
-    //build popup content string starting with city...Example 2.1 line 24
-    var popupContent = "<p><b>City:</b> " + feature.properties.city + "</p>";
-    
-    //add formatted attribute to popup content string
-    var year = attribute.split("_")[1];
-    popupContent += "<p><b>Population in " + year + ":</b> " + feature.properties[attribute] + "</p>";
-    
-    //bind the popup to the circle marker
-    layer.bindPopup(popupContent, {
-          offset: new L.Point(0,-options.radius)
-      });
-    
-    //return the circle marker to the L.geoJson pointToLayer option
-    return layer;
-  };
-
-function createPropSymbols(data, attributes){
-    //create a Leaflet GeoJSON layer and add it to the map
-    L.geoJson(data, {
-        pointToLayer: function(feature, latlng){
-            return pointToLayer(feature, latlng, attributes);
-        }
-    }).addTo(map);
-};
-
-//Resize proportional symbols according to new attribute values
-function updatePropSymbols(attribute){
-    map.eachLayer(function(layer){
-        if (layer.feature && layer.feature.properties[attribute]){
-          //access feature properties
-           var props = layer.feature.properties;
-           //update each feature's radius based on new attribute values
-           var radius = calcPropRadius(props[attribute]);
-           layer.setRadius(radius);
-           //add city to popup content string
-           var popupContent = "<p><b>City:</b> " + props.city + "</p>";
-           //add number of bars attribute to panel content string
-           var year = attribute.split("_")[1];
-           popupContent += "<p><b>Population in " + year + ":</b> " + props[attribute] + "</p>";
-           //update popup with new content
-           popup = layer.getPopup();
-           popup.setContent(popupContent).update();
-        };
-    });
-};
-
-function processData(data){
-    //empty array to hold attributes
-    var attributes = [];
-    //properties of the first feature in the dataset
-    var properties = data.features[0].properties;
-    //push each attribute name into attributes array
-    for (var attribute in properties){
-        //only take attributes with population values
-        if (attribute.indexOf("Pop") > -1){
-            attributes.push(attribute);
-        };
-    };
-    return attributes;
-};
-
-//Create new sequence controls
-function createSequenceControls(attributes){
-    //create range input element (slider)
-    $('#panel').append('<input class="range-slider" type="range">');
-    //set slider attributes
-    $('.range-slider').attr({
-        max: 12,
-        min: 0,
-        value: 0,
-        step: 1
-    });
-    //add step buttons
-    $('#panel').append('<button class="step" id="reverse">Reverse</button>');
-    $('#panel').append('<button class="step" id="forward">Forward</button>');
-  //replace button content with images
-   $('#reverse').html('<img src="img/reverse.png">');
-   $('#forward').html('<img src="img/forward.png">');
-   //click listener for buttons
-    $('.step').click(function(){
-      //get the old index value
-      var index = $('.range-slider').val();
-
-      //increment or decrement depending on button clicked
-      if ($(this).attr('id') == 'forward'){
-          index++;
-          //if past the last attribute, wrap around to first attribute
-          index = index > 12 ? 0 : index;
-      } else if ($(this).attr('id') == 'reverse'){
-          index--;
-          //if past the first attribute, wrap around to last attribute
-          index = index < 0 ? 12 : index;
-      };
-
-      //update slider
-      $('.range-slider').val(index);
-      //pass new attribute to update symbols
-        updatePropSymbols(attributes[index]);
-    });
-
-    //input listener for slider
-    $('.range-slider').on('input', function(){
-      //get the new index value
-      var index = $(this).val();
-      //pass new attribute to update symbols
-        updatePropSymbols(attributes[index]);
-    });
-};
-
-//Step 2: Import GeoJSON data
+//function to retrieve the data and place it on the map
 function getData(map){
     //load the data
     $.getJSON("data/US_Cities.geojson", function(response){
-            var attributes = processData(response);
-            //calculate minimum data value
-            minValue = calculateMinValue(response);
-            //call function to create proportional symbols
-            createPropSymbols(response);
-            createSequenceControls(attributes);
-    });
-};
-
+            var geojsonMarkerOptions = {
+            radius: 8,
+            fillColor: "#ff7800",
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+            };
+    
+    L.geoJSON(response, {
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+    }}).addTo(map);
+        
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(response, {
+                onEachFeature: onEachFeature
+            }).addTo(map);
+        
 $(document).ready(createMap);
