5bbb0dd6605e12a34e5cc0710c9a8360bb73efc7
teddynguyen2 <61038278+teddynguyen2@users.noreply.github.com>
2021-03-10 21:24:38 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -16,6 +16,21 @@ function createMap(){
 
     //call getData function
     getData(map);
+    
+    /*Legend specific*/
+    var legend = L.control({ position: "bottomright" });
+
+    legend.onAdd = function(map) {
+        var div = L.DomUtil.create("div", "legend");
+        div.innerHTML += "<h4>Tegnforklaring</h4>";
+        div.innerHTML += '<i style="background: #477AC2"></i><span>Water</span><br>';
+        div.innerHTML += '<i style="background: #448D40"></i><span>Forest</span><br>';
+        div.innerHTML += '<i style="background: #E6E696"></i><span>Land</span><br>';
+        div.innerHTML += '<i style="background: #E8E6E0"></i><span>Residential</span><br>';
+        div.innerHTML += '<i style="background: #FFFFFF"></i><span>Ice</span><br>';
+        div.innerHTML += '<i class="icon" style="background-image: url(https://d30y9cdsu7xlg0.cloudfront.net/png/194515-200.png);background-repeat: no-repeat;"></i><span>Grænse</span><br>';
+        return div;
+        }; legend.addTo(map);
 };
 
 function calculateMinValue(data){
@@ -197,4 +212,5 @@ function getData(map){
 };
 
 $(document).ready(createMap);
+    
 
