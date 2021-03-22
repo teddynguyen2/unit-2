13ece5185b6b781ecc5956292e9db81d92f07839
teddynguyen2 <61038278+teddynguyen2@users.noreply.github.com>
2021-03-21 20:46:52 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -60,9 +60,9 @@ function calculateMinValue(data){
 function calcPropRadius(attValue) {
     if (attValue >= 1){
         //constant factor adjusts symbol sizes evenly
-        var minRadius = 0.5;
+        var minRadius = 5;
         //Flannery Apperance Compensation formula
-        var radius = 1.0083 * Math.pow(attValue/minValue,0.5715) * minRadius
+        var radius = 0.1 * Math.pow(attValue/minValue,0.5715) * minRadius
         return radius;
     }
     else {
